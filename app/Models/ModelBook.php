97eebaceee22c2815b39730b4401f6f6eb758ca6
sergiofilhoja <sergiofilhoja@hotmail.com>
2020-08-25 12:56:59 -0300

    CRUD

---
+++ app/Models/ModelBook.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+
+class ModelBook extends Model
+{
+    protected $table = 'book';
+
+    public function relUsers()
+    {
+        return $this->hasOne('App\User', 'id', 'id_user');
+    }
+}
